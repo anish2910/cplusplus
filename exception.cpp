bcb95a5ef91384f80f4331d158d623c5328fe927
anish2910 <90914492+anish2910@users.noreply.github.com>
2022-08-04 11:11:57 +0530

    Create exception.cpp

---
+++ exception.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+double division(int var1, int var2)
+{
+    if (var2 == 0) {
+        throw "Division by Zero.";
+    }
+    return (var1 / var2);
+}
+
+int main()
+{
+    int a = 30;
+    int b = 0;
+    double d = 0;
+
+    try {
+        d = division(a, b);
+        cout << d << endl;
+    }
+    catch (const char* error) {
+        cout << error << endl;
+    }
+
+    retu
